6af4fbd4fc6e4abef015c175b60108d4c5c2f256
kasper95 <kove_kasper@outlook.com>
2017-05-22 22:01:10 -0400

    inital commit

---
+++ src/main/java/com/kasperskove/giflib/GiflibApplication.java
@@ -0,0 +1,12 @@
+package com.kasperskove.giflib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiflibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiflibApplication.class, args);
+	}
+}
