281ef784d89b555309b0eda34c6bc193a3a17f93
kasper95 <kove_kasper@outlook.com>
2017-05-24 23:43:43 -0400

    +Category, +CategoryRepository

---
+++ src/main/java/com/kasperskove/giflib/utilities/CategoryRepository.java
@@ -0,0 +1,21 @@
+package com.kasperskove.giflib.utilities;
+
+import com.kasperskove.giflib.entities.Category;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class CategoryRepository {
+
+    private static final List<Category> ALL_CATEGORIES = Arrays.asList(
+            new Category(1, "Technology"),
+            new Category(2, "People"),
+            new Category(3, "Destruction")
+    );
+
+    public List<Category> getAllCategories (){
+        return ALL_CATEGORIES;
+    }
+}
